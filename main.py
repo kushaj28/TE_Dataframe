fe9c163f7920ac126868c17f0777f6ce0695a466
kushaj28 <88649696+kushaj28@users.noreply.github.com>
2021-08-09 10:59:11 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd
+import json
+url="https://takaful-interview-969lesz9nxfe.runkit.sh"
+param1={"gender":"male"}
+male_req=requests.get(url,params=param1)
+param2={"gender":"female"}
+female_req=requests.get(url,params=param2)
+data_male=male_req.json()
+data_female=female_req.json()
+print(type(data_male))
+print(data_male)
+print(data_female)
+data = pd.DataFrame([data_male,data_female],index=[1,2])
+
+
+print (data)
